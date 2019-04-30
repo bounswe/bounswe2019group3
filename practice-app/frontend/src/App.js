e05bdbbd346d4f4a787a88f87c4644fcf90c0abc
hilal-demir <hll9694@hotmail.com>
2019-04-30 15:35:26 +0300

    added logo

--- practice-app/frontend/src/App.js
+++ practice-app/frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div>
-
+      <img src={logo} alt="logo" align="middle" width="200" height="200" />
     </div>
   );
 }
