52bb49d4ae29bfdf55c327e7c14874f93a0ced5c
hilal-demir <hll9694@hotmail.com>
2019-04-30 17:30:19 +0300

    lyricsSongSearch changes

--- practice-app/frontend/src/LyricsSongSearch.js
+++ practice-app/frontend/src/LyricsSongSearch.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import axios from 'axios';
+import React, {useState} from 'react';
+
+let name="name";
+let artist="artist";
+let album="album";
+let type="type";
+let rating="rating";
+let lyrics="yes";
+let subtitles="yes";
+
 
 
 function LyricsSongSearch(props) {
+    
+    const [data,setData] = useState([]);
+    
+    function songSearch() {
+    axios.get('http://localhost:3000/lyrics_song_search?str=' + props.word)
+    .then((res) => setData(res.data) 
+    .catch((e) => console.log("error",e));
+
+  }
     return (
-        <div>LyricsSongSearch word:{props.word}</div>
+        <div>
+        <div><b>LyricsSongSearch word: </b>{props.word}</div>
+        <div><b>Name: </b>{name}</div>
+        <div><b>Artist: </b>{artist}</div>
+        <div><b>Album: </b>{album}</div>
+        <div><b>Type: </b>{type}</div>
+        <div><b>Rating: </b>{rating}</div>
+        <div><b>Lyrics: </b>{lyrics}</div>
+        <div><b>Subtitles: </b>{subtitles}</div>
+        </div>
     );
 }
 
