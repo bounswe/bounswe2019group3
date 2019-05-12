344e2ecf7e769d7d506b7105a4053fd3d5551321
Samet Demir <demir.samet@hotmail.com>
2019-05-12 21:12:33 +0300

    start image search backend

--- practice-app/backend/src/main/java/bounswe2019group3/implementation_assignment/ImageSearchController.java
+++ practice-app/backend/src/main/java/bounswe2019group3/implementation_assignment/ImageSearchController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ImageSearchController {
 
-    //@CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping("/image_search")
     public String image_search(@RequestParam(value="str", defaultValue="") String str) {
-        return "Not Implemented";
+        RestTemplate template= new RestTemplate();
+        String url="https://api.giphy.com/v1/gifs/translate?api_key=plXziMnPirNFvdURRItWfDJmAyPHvkDV&s="+str;
+        
+        String output="";
+        output=template.getForObject(url, String.class);
+        
     }
 }
