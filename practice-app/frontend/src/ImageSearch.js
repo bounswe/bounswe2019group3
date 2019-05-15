63d399aa23a94fac8b5032e52bc4e800e79c87d3
Samet Demir <demir.samet@hotmail.com>
2019-05-15 04:41:56 +0300

    fix problems about image search

--- practice-app/frontend/src/ImageSearch.js
+++ practice-app/frontend/src/ImageSearch.js
@@ -3,21 +3,26 @@ import React, {useState} from 'react';
 import {getDomain} from './utility'
 
 function ImageSearch(props) {
-    const [data,setData] = useState([]);
+    const [data,setData] = useState(undefined);
     const [word,setWord] = useState("");
     
     if(props.word && props.word != word){
       setWord(props.word);
       axios.get(getDomain() + '/image_search?str=' + props.word)
-      .then((res) => setData(res.data || []))
+      .then((res) => setData(res.data))
       .catch((e) => console.log("error: ",e));
     }
+    console.log("data", data);
+
+    let iframe = <div/>
+    if(data){
+      iframe = <iframe src={data} width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+    }
+
     return (
         <div>
-        <div><b><u>Images</u></b></div>
-        <div style={{ width: "100%" }} style={{ height: "0" }} style={{ paddingBottom: "69%" }} style={{ position: "relative" }}><iframe src={data} height="100%" width="100%" style={{position:"absolute"}} frameBorder="0" allowFullScreen></iframe></div><p><a href={data}>via GIPHY</a></p>
-     
-          
+          <div><b><u>Images</u></b></div>
+          {iframe}
         </div>
        
     );
