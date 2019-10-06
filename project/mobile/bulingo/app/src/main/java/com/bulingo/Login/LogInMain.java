08e77319c087bab3d94df5b82d11fded724fc0d4
yagmurkahyaoglu <kahyaoglu.yagmur@gmail.com>
2019-10-06 15:25:13 +0300

    File rearrangement

---
+++ project/mobile/bulingo/app/src/main/java/com/bulingo/Login/LogInMain.java
@@ -0,0 +1,16 @@
+package com.bulingo.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bulingo.R;
+
+public class LogInMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_main);
+    }
+}
