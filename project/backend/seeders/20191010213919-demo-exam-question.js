d02617e8a9b9cc9421c44a4c4be0bf75f10d96aa
Yusuf Sabri Bayrakdar <32673244+yusufbayrakdar@users.noreply.github.com>
2019-10-12 17:34:28 +0300

    Questions are added in Deutsch section

--- project/backend/seeders/20191010213919-demo-exam-question.js
+++ project/backend/seeders/20191010213919-demo-exam-question.js
@@ -3,6 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('ExamQuestions',[
+      // ENGLISH
       {
           id:         0,
           lang_abbr:  "en",
@@ -42,13 +43,53 @@ module.exports = {
           answer_id:  2,
           createdAt:  new Date(),
           updatedAt:  new Date()
+      },
+      // DEUTSCH
+      {
+          id:         5,
+          lang_abbr:  "de",
+          desc:       "----, sondern benötigen meist Monate oder Jahre, um sichtbar zu werden.",
+          answer_id:  1,
+          createdAt:  new Date(),
+          updatedAt:  new Date()
+      },
+      {
+        id:         6,
+        lang_abbr:  "de",
+        desc:       "Selbst wenn die Kohlenstoffdioxid-Emissionen heute drastisch reduziert würden, ----.",
+        answer_id:  0,
+        createdAt:  new Date(),
+        updatedAt:  new Date()
+      },
+      {
+        id:         7,
+        lang_abbr:  "de",
+        desc:       "Während etwa ein 40-jähriger Lehrer noch mit 40 weiteren Jahren Lebenszeit rechnen kann, ----.",
+        answer_id:  2,
+        createdAt:  new Date(),
+        updatedAt:  new Date()
+      },
+      {
+        id:         8,
+        lang_abbr:  "de",
+        desc:       "Armut und Rückständigkeit sind eine der gefährlichsten Herausforderungen, ----.",
+        answer_id:  2,
+        createdAt:  new Date(),
+        updatedAt:  new Date()
+      },
+      {
+        id:         9,
+        lang_abbr:  "de",
+        desc:       "Ein neugeborenes Rehkitz wird so gut wie nie von Fressfeinden angegriffen, ----.",
+        answer_id:  0,
+        createdAt:  new Date(),
+        updatedAt:  new Date()
       }
-  ], {});
+  ]);
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('ExamQuestions', 
-      {id: {[Sequelize.Op.in]: [0 , 1]}}
-      , {});
+      {id: {[Sequelize.Op.in]: [0 , 1]}});
   }
 };
