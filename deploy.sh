cd2c404d1b425d9fd3bf7c23feda0660b328cbc6
Samet Demir <demir.samet@hotmail.com>
2019-10-12 11:08:37 +0300

    fix config

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ cd ../..
 sudo rm -rf bounswe2019group3
 git clone --depth=50 --branch=production https://github.com/bounswe/bounswe2019group3.git
 cd bounswe2019group3/project
-docker-compose up -d
+docker-compose up -d --force-recreate --build
