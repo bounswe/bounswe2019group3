3619c95934a44771f90586d4cbbb15478afc8417
Yusuf Sabri Bayrakdar <32673244+yusufbayrakdar@users.noreply.github.com>
2019-10-12 04:35:01 +0300

    evaluation is completed

--- project/backend/routes/Language.js
+++ project/backend/routes/Language.js
@@ -58,16 +58,65 @@ router.get('/:language_abbr/exam/questions', (req, res, next) => {
 
 
 /**
- * @api {post} /api/languague/:language_abbr/exam/evaluate evaluates level determination exam
+ * @api {post} /api/language/:language_abbr/exam/evaluate evaluates level determination exam
  * @apiName exam/evaluate
  * @apiGroup language
  * @apiPermission User
  * @apiParam (Request body(JSON)) {Object[]} answers                    list of answers
  * @apiParam (Request body(JSON)) {String}   answers.question_id       question id
- * @apiParam (Request body(JSON)) {Object[]} answers.choices_id        choices id
+ * @apiParam (Request body(JSON)) {String} answers.choices_id        choices id
+ * @apiSuccess {String}   grade  result of evaluation
  */
 router.post('/:language_abbr/exam/evaluate', (req, res, next) => {
-    res.sendStatus(501);
+    if(!req.params.language_abbr){
+        res.sendStatus(400);
+    } else {
+        const db = req.db;
+        var question_id_array = new Array();
+        var choice_id_array = new Array();
+        for (let index = 0; index < req.body.length; index++) {
+            question_id_array[index] = req.body[index].question_id;
+            choice_id_array[index] = req.body[index].choice_id;
+        }
+        db.ExamQuestion.findAll({
+            where: db.Sequelize.or(
+                { 
+                    id: question_id_array,
+                }
+            )
+        })
+        .then((question) => {
+            let counter = 0;
+            for (let i = 0; i < question.length; i++) {
+                let index = question_id_array.findIndex(search_item => search_item == question[i].id);
+                if(question[i].answer_id == choice_id_array[index]){
+                    counter ++;
+                }
+            }
+            const success_rate = counter / question.length;
+            let grade;
+            if (success_rate==0) {
+                grade = 'A1'
+            }
+            else if (success_rate<=0.2) {
+                grade = 'A2'
+            }
+            else if (success_rate<=0.4) {
+                grade = 'B1'
+            }
+            else if (success_rate<=0.6) {
+                grade = 'B2'
+            }
+            else if (success_rate<=0.8) {
+                grade = 'C1'
+            }
+            else if (success_rate==1) {
+                grade = 'C2'
+            }
+            res.send({grade});
+    });
+    }
+    
 });
 
 /**
