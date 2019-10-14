213a6ec433600961b36064aa0fa6108b29c91a9a
burak ilkay akgün <44066588+burakilkayakgun@users.noreply.github.com>
2019-10-15 00:02:31 +0300

    database connection is provided

--- project/frontend/src/EnglishTest.js
+++ project/frontend/src/EnglishTest.js
@@ -7,9 +7,33 @@ import axios from 'axios';
 export default class EnglishTest extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { showPopup: false };  
-      }
+        axios.get('http://ec2-52-59-191-167.eu-central-1.compute.amazonaws.com/api/language/en/exam/questions')
+      .then(res => {
+        this.setState({questions: res.data});
+      })
+        this.state = {
+            questions : []
+        }
+    }
     
+      generateQuestions(){
+        var ques = [];
+        for(let i=0 ; i < this.state.questions.length ; i++){
+            var questionAnswer = [];
+            for(var j=0;j<this.state.questions[i].choices.length;j++){
+                questionAnswer[j]=this.state.questions[i].choices[j].desc;
+            }
+            ques[i] = (
+                <Question 
+                id={this.state.questions[i].id} 
+                question={(i+1)+ ". " +this.state.questions[i].desc} 
+                answers={questionAnswer}
+               />
+            );
+        }
+        return ques;
+    }
+
        onClickd() {
         const frm = {
          //this json will be send to database
@@ -18,13 +42,7 @@ export default class EnglishTest extends React.Component {
             showPopup: !this.state.showPopup  
        });  
         console.log(JSON.stringify(frm));
-        axios.post('http://ec2-52-59-191-167.eu-central-1.compute.amazonaws.com/api/auth/signup',  frm )
-            .then(res => {
-              console.log(res);
-              if (res.status == 200) {
-                console.log(this);
-              }
-            })
+        
       
       };
     
@@ -47,9 +65,8 @@ export default class EnglishTest extends React.Component {
                     
                     <MDBCol md="6">
 
-                        <Question id={1} question={"What are you giving Max for his birthday?"} answers={["A: I give him a gold watch.", "B: I'm giving a gold watch him.", "C: I'm giving him a few money.", "D: I'm giving him this antique watch."]} /><br/>
-                        <Question id={2} question={"I was driving ______ the road when my car started making a funny sound."} answers={["in", "at", "away", "down"]} /><br/>
-                        <Question id={3} question={"Whose keys are these?  ______ are mine."} answers={["A: They", "B: These", "C: It", " E: Whose"]} /><br/>
+                        { this.generateQuestions() }
+
                         <MDBBtn color="orange" onClick={this.onClickd.bind(this)} className="text2"> Submit</MDBBtn>
                     </MDBCol>
                     <MDBCol md="3"></MDBCol>
