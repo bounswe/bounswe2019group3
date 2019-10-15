f75dabfbccb245d257d54566f2e2d7cad0c26ef0
Samet Demir <demir.samet@hotmail.com>
2019-10-15 21:21:16 +0300

    improve some config

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -6,8 +6,8 @@ import 'mdbreact/dist/css/mdb.css';
 import SignUp from "./containers/SignUp";
 import App from "./App";
 import ProfilePage from "./containers/ProfilePage";
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
-serviceWorker.register();
+//serviceWorker.register();
