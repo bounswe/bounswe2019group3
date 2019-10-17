96ab0ab5e571b1bf5b9b28cff7ac79ccb987d6eb
Ebru Zorlu <ebru.zorlu@boun.edu.tr>
2019-10-17 17:07:11 +0300

    profile page

---
+++ project/mobile/bulingo/app/src/main/java/com/bulingo/Profile/ProfilePage.java
@@ -0,0 +1,14 @@
+package com.bulingo.Profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page);
+    }
+}
