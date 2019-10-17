5cfe278e3f5ebeae464ba35bfea65134e3c512d7
Berkay Özerbay <berkayozerbay@hotmail.com>
2019-10-18 00:14:18 +0300

    added level.js, not finished but probably works

---
+++ project/backend/models/level.js
@@ -0,0 +1,27 @@
+'use strict';// not finished, just a skeleton
+module.exports = (sequelize, DataTypes) => {
+  const Level = sequelize.define('Level', {
+    lang_abbr: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.STRING
+    },
+    grade	: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.STRING
+      },
+
+
+  }, {});
+Level.associate = function(models) {
+    models.Level.belongsTo(models.UserProfile, {
+      foreignKey: 'user_id',
+      as: 'user',
+      constraints: false
+  });
+  };
+  return Level;
+};
