59ba0b2606c06909cffcae606d7b49cd5e648959
burakilkayakgun <burakilkayakgun@gmail.com>
2019-10-19 01:01:50 +0300

    minor update

--- project/frontend/src/ProfilePage.js
+++ project/frontend/src/ProfilePage.js
@@ -97,8 +97,8 @@ export default class FormPage extends React.Component {
           <MDBCol md="2"></MDBCol>
           <MDBCol md="3"><div className="Scrollbar topMargined">
             <div > <p className="text-blue" > Welcome <i>{Cookies.get('username')}</i></p> </div>
-            <div > <p className="text-blue" > Last exam taken language: <i>{Cookies.get('selectedExamLanguage')}</i> </p> </div>
-            <div > <p className="text-blue" > Grade: <i>{Cookies.get('selectedExamGrade')}</i></p> </div>
+            <div > <p className="text-blue" > Last exam taken language <i>{Cookies.get('selectedExamLanguage')}</i> </p> </div>
+            <div > <p className="text-blue" > Grade <i>{Cookies.get('selectedExamGrade')}</i></p> </div>
             <div className="Bio"><p>Bio</p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque in dictum leo. Vestibulum sed lacus ac tellus consectetur bibendum. Fusce ipsum dui, feugiat at eros in, auctor luctus tellus. Quisque at vehicula leo. Praesent vitae felis elementum, luctus libero dapibus, rhoncus ligula. Donec aliquam augue sed nunc suscipit consectetur. Integer sed sollicitudin ligula, vitae porta nisl. Vivamus dapibus diam cursus, porttitor ante ut, consequat est. </div>
 
           </div></MDBCol>
