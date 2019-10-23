966f563ee25a2134ba0f40a80f4bd35e69ae8477
Neval Tüllük <nevaltulluk@gmail.com>
2019-10-23 20:25:31 +0300

    exam page update

--- project/frontend/src/GeneralTest.js
+++ project/frontend/src/GeneralTest.js
@@ -4,11 +4,12 @@ import Popup from './Popup';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import Cookies from 'js-cookie'
+import './SignUp.css';
 
 export default class GeneralTest extends React.Component {
     constructor(props) {
         super(props);
-        axios.get('http://18.184.207.248/api/language/' + Cookies.get('selectedExamLanguageAbbr') + '/exam/questions',{withCredentials: true})
+        axios.get('http://18.184.207.248/api/language/' + Cookies.get('selectedExamLanguageAbbr') + '/exam/questions', { withCredentials: true })
             .then(res => {
                 console.log(res.data);
                 this.setState({ questions: res.data });
@@ -23,24 +24,24 @@ export default class GeneralTest extends React.Component {
         }
     }
     onClickd(id, qid, arr) {
-        if(Cookies.get('selectedExamLanguageAbbr') === 'de'){
-            arr[qid] = { question_id: "" + (qid+5), choice_id: "" + id };
-        }else{
+        if (Cookies.get('selectedExamLanguageAbbr') === 'de') {
+            arr[qid] = { question_id: "" + (qid + 5), choice_id: "" + id };
+        } else {
             arr[qid] = { question_id: "" + qid, choice_id: "" + id };
         }
-        
+
         console.log(arr);
     }
     generateQuestions(ans) {
-        
+
         var ques = [];
 
-         for (let i = 0; i < this.state.questions.length; i++) {
-             ans[i] = { question_id: "" + this.state.questions[i].id, choice_id: undefined };
-         }
+        for (let i = 0; i < this.state.questions.length; i++) {
+            ans[i] = { question_id: "" + this.state.questions[i].id, choice_id: undefined };
+        }
         //var questionAnswer = [];     
         for (let i = 0; i < this.state.questions.length; i++) {
-            
+
             ques[i] = (
 
                 //<Question 
@@ -49,28 +50,30 @@ export default class GeneralTest extends React.Component {
                 //answers={questionAnswer}
                 //choice_id = {this.state.choice_id}           
                 ///>
-                <form>
-                    <fieldset className="question text3">{(i + 1) + ". " + this.state.questions[i].desc}<br />
-                        <div className="answers" >
-                            <div>
-                                <input type="radio" id="1" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[0].id, i, ans)} />
-                                <label for="a">{this.state.questions[i].choices[0].desc}</label>
+                <div className="ExamBox">
+                    <form>
+                        <fieldset className="question"><p className="commentsec_title">{(i + 1) + ". " + this.state.questions[i].desc}</p>
+                            <div className="answers radio-toolbar" >
+                                <div className = "answerstext">
+                                    <label for="a"><input type="radio" id="1" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[0].id, i, ans)} />
+                                    {this.state.questions[i].choices[0].desc}</label>
+                                </div>
+                                <div className = "answerstext">
+                                    <label for="b"><input type="radio" id="2" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[1].id, i, ans)} />
+                                    {this.state.questions[i].choices[1].desc}</label>
+                                </div>
+                                <div className = "answerstext">  
+                                    <label for="c"><input type="radio" id="3" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[2].id, i, ans)} />
+                                    {this.state.questions[i].choices[2].desc}</label>
+                                </div>
+                                <div className = "answerstext">
+                                    <label for="d"><input type="radio" id="4" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[3].id, i, ans)} />
+                                    {this.state.questions[i].choices[3].desc}</label>
+                                </div>
                             </div>
-                            <div>
-                                <input type="radio" id="2" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[1].id, i, ans)} />
-                                <label for="b">{this.state.questions[i].choices[1].desc}</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="3" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[2].id, i, ans)} />
-                                <label for="c">{this.state.questions[i].choices[2].desc}</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="4" name={i} onClick={this.onClickd.bind(this, this.state.questions[i].choices[3].id, i, ans)} />
-                                <label for="d">{this.state.questions[i].choices[3].desc}</label>
-                            </div>
-                        </div>
-                    </fieldset>
-                </form>
+                        </fieldset>
+                    </form>
+                </div>
 
             );
 
@@ -81,7 +84,7 @@ export default class GeneralTest extends React.Component {
 
 
     submit(ans) {
-        axios.post('http://18.184.207.248/api/language/' + Cookies.get('selectedExamLanguageAbbr') + '/exam/evaluate', ans, {withCredentials: true})
+        axios.post('http://18.184.207.248/api/language/' + Cookies.get('selectedExamLanguageAbbr') + '/exam/evaluate', ans, { withCredentials: true })
             .then(res => {
                 if (res.status === 200) {
                     console.log(ans);
@@ -133,15 +136,15 @@ export default class GeneralTest extends React.Component {
 
                 <MDBRow className="topMargined50">
                     <center><img className="backpicture" src=".\earth3.png" alt="." width="80%" /></center>
-                    <MDBCol md="3"></MDBCol>
+                    <MDBCol md="2"></MDBCol>
 
-                    <MDBCol md="6">
+                    <MDBCol md="8">
 
                         {this.generateQuestions(ans)}
 
                         <MDBBtn color="green" onClick={this.submit.bind(this, ans)} className="text2"> Submit</MDBBtn>
                     </MDBCol>
-                    <MDBCol md="3"></MDBCol>
+                    <MDBCol md="2"></MDBCol>
                 </MDBRow>
             </MDBContainer>
         );
