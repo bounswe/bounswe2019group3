9c0da7a69773c4b564b97a762ab7329e403852e0
Neval Tüllük <nevaltulluk@gmail.com>
2019-11-11 23:13:47 +0300

    Update Messages.js

--- project/frontend/src/Messages.js
+++ project/frontend/src/Messages.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput,MDBIcon } from 'mdbreact';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput, MDBIcon } from 'mdbreact';
 import './SignUp.css';
 import { Redirect, NavLink } from 'react-router-dom';
 import Cookies from 'js-cookie'
@@ -9,53 +9,38 @@ import axios from 'axios';
 export default class Messages extends React.Component {
     constructor(props) {
         super(props);
+        axios.get('http://18.184.207.248/api/chat/', { withCredentials: true })
+      .then(res => {
+        //console.log(res.data);
+        this.setState({ last_messages: res.data });
+        console.log(this)
+      })
+      this.state = {
+          last_messages : {}
+    }
         
-      
-        /* axios.get('http://18.184.207.248/api/chat/')
-          .then(res => {
-            console.log(res.data);
-            this.setState({ information: res.data });
-          }) */
-    
-        this.state = {
-          
-          information: {
-            "nb_new_messages": 3,
-            "history": [
-               {
-                   "username": "user",
-                   "last_message": "hello world",
-                   "nb_new_messages": 1,
-                   "last_message_date": "2013-10-21T13:28:06.419Z"
-               },
-               {
-                   "username": "admin",
-                   "last_message": "welcome to bulingo",
-                   "nb_new_messages": 2,
-                   "last_message_date": "2013-10-20T11:10:04.222Z"
-               }    
-            ]
-          },
-          
-        }
-      }
-    messageField(){
+    }
+    messageField() {
         var messages = [];
-        for (let i = 0; i < this.state.information.history.length; i++) {
-            messages[this.state.information.history.length - i] = (
-            <div className="Messagebox" onClick={this.openmessages.bind(this)}>          
-            <p>{this.state.information.history[i].username}</p>
-            <p>{this.state.information.history[i].last_message}</p>
-            </div>
-        );
+        //console.log(this.state);
+        for (let i = 0; i < this.state.last_messages.history.length; i++) {
+            messages[this.state.last_messages.history.length - i] = (
+                <div className="Messagebox" onClick={this.openmessages.bind(this.state.last_messages.history[i].username)}>
+                    <p>{this.state.last_messages.history[i].username}</p>
+                    <p>{this.state.last_messages.history[i].last_message}</p>
+                </div>
+            );
         }
         return messages;
     }
-    messagehistorywindow(user){}
-    sendmessage(){}
-    openmessages(){
-        console.log("im_clicked")
+    openmessages(usr) {
+        axios.get('http://18.184.207.248/api/chat/'+ usr)
+            .then(res => {
+                //console.log(res.data);
+            })
     }
+    sendmessage() {}
+    
     render() {
         if (this.state.isLogout) {
             return (<Redirect
@@ -74,24 +59,24 @@ export default class Messages extends React.Component {
                         </div>
                     </MDBCol>
                     <MDBCol md="7">
-                       <MDBRow>
-                           <div className = "messagehistory Scrollbar"></div>
-                       </MDBRow>
-                       <MDBRow>
-                       
-                       <textarea
-                type="text"
-                className="form-control top-margined-20"
-                rows="2"
-              />
-              <div className="mt-4">
-                <MDBBtn color="orange" onClick={this.sendmessage.bind(this)} background type="submit">
-                  SEND
+                        <MDBRow>
+                            <div className="messagehistory Scrollbar"></div>
+                        </MDBRow>
+                        <MDBRow>
+
+                            <textarea
+                                type="text"
+                                className="form-control top-margined-20"
+                                rows="2"
+                            />
+                            <div className="mt-4">
+                                <MDBBtn color="orange" onClick={this.sendmessage.bind(this)} type="submit">
+                                    SEND
                 <MDBIcon far icon="paper-plane" className="ml-2" />
-                </MDBBtn>
-              </div>
-                       </MDBRow>
-                       <MDBRow></MDBRow>
+                                </MDBBtn>
+                            </div>
+                        </MDBRow>
+                        <MDBRow></MDBRow>
                     </MDBCol>
                     <MDBCol md="2"></MDBCol>
                 </MDBRow>
