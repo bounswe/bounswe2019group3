fc9d628a9c3e673f290c600070a8901d89a0a342
burak ilkay akgun <ilkay.akgun1@boun.edu.tr>
2019-11-15 21:10:22 +0300

    search bar is added to profile page

--- project/frontend/src/ProfilePage.js
+++ project/frontend/src/ProfilePage.js
@@ -94,7 +94,8 @@ export default class FormPage extends React.Component {
     '<li id="chld"><a href="/writing">Send Writing</a></li>'+
     '<li id="chld"><a href="/messages">Messages</a></li>' +
     '<li id="chld" style="float:right";><a href="/Logout">Logout</a></li>'+
-    '<li id="chld" style="float:right";><a href="/Settings" >Settings</a></li>');
+    '<li id="chld" style="float:right";><a href="/Settings" >Settings</a></li>'+
+    '<li style="float: right;"><div class=" search-container"><form action="/#"><input type="text" placeholder="Search.." name="search" /><button type="submit"><i class="fa fa-search"></i></button></form></div></li>');
   }
 
   render() {
