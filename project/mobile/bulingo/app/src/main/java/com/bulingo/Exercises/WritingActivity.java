5e29fce3774efe6de013c3ee79cdbd8f545238a0
orenbartu <bartu.oren@boun.edu.tr>
2019-12-19 15:39:28 +0300

    Writing Exercise UI

---
+++ project/mobile/bulingo/app/src/main/java/com/bulingo/Exercises/WritingActivity.java
@@ -0,0 +1,20 @@
+package com.bulingo.Exercises;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.bulingo.R;
+
+public class WritingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writing);
+    }
+
+    public void selectImage(View view) {
+    }
+}
