b5787f4eebc980d6d41cfca0d96919c6107dfd3e
orenbartu <bartu.oren@boun.edu.tr>
2019-12-22 10:51:39 +0300

    Sent Received Writing Layout

---
+++ project/mobile/bulingo/app/src/main/java/com/bulingo/Exercises/SendReceiveWriting.java
@@ -0,0 +1,16 @@
+package com.bulingo.Exercises;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bulingo.R;
+
+public class SendReceiveWriting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_receive_writing);
+    }
+}
