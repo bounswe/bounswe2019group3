c5ee31b9548d02e49cfca15cb7677d36fd7006be
Neval Tüllük <nevaltulluk@gmail.com>
2019-12-22 21:42:24 +0300

    annotation update

--- project/frontend/src/writingsRead.js
+++ project/frontend/src/writingsRead.js
@@ -1,31 +1,101 @@
 import React from 'react';
-import { MDBContainer, MDBRow, MDBCol } from 'mdbreact';
+import { Card, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 import './General.css';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import Cookies from 'js-cookie'
+import { TokenAnnotator, TextAnnotator } from 'react-text-annotate'
+import { State, Toggle } from 'react-powerplug'
+
 
 var content;
 export default class FormPage extends React.Component {
   constructor(props) {
-    super(props);
-
+    super(props);    
     content = this.props.location._data;
+    //http://18.184.207.248/api/annotation/?target_id=http://18.184.207.248/api/writing/213312
+    axios.get('http://18.184.207.248/api/annotation/?target_source=http://18.184.207.248/api/writing/' + content.writing_id, { withCredentials: true })
+            .then(res => {
+                console.log(res.data.length);
+                var temp_data = [];
+                for (let i = 0; i < res.data.length; i++) {
+                    var temp = res.data[i].target.selector.value.substring(5);
+                    temp_data[i] = 
+                         {
+                            start: parseInt(temp.split(',')[0]),
+                            end: parseInt(temp.split(',')[1]),
+                            tag: res.data[i].body.value
+                    }
+                }
+                this.setState({
+                    text_anno_data:temp_data
+                })                
+            })
+        this.state ={
+            text_anno_data:[]
+        }
 
   }
-  
-  fill_writing_table() {
+  myonFocus(element) {
+    element.target.value = "";
+  }
+  textWriting() {
+    console.log([this.state.text_anno_data]);
+    
+    var row = [];
+    
+    row[0] = (
+      <div>
+        <center> <div className="commentsec_title "> {content.title}  </div></center>
+        <MDBRow>
+          <br />
+          <State initial={{ value: [this.state.text_anno_data], tag: '' }}>
+            {({ state, setState }) => (
+              <Card className="topMargined">
+
+                <textarea onChange={e => setState({ tag: e.target.value })} onFocus={this.myonFocus.bind(this)}
+                  class="form-control rounded-0" id="anno" rows="3" placeholder="annotation"></textarea>
+                <div className="topMargined">
+                  <TextAnnotator
+                    style={{
+                      maxWidth: 500,
+                      lineHeight: 1.5,
+                    }}
+                    content={content.text}
+                    value={state.value}
+                    onChange={value => setState({ value })}
+                    getSpan={span => ({
+                      ...span,
+                      tag: state.tag,
+                      color: "yellow",
+                      value: "",
+                    })}
+                  />
+                </div>
+              </Card>
+            )}
+          </State>
+        </MDBRow>
+      </div>
+
+    );
+
+    return row;
+  }
+  imageWriting() {
     var row = [];
         row[0] = (
             <div>
+                <img className="topMargined marginedleft50" src={'http://18.184.207.248/'+content.image} alt="." />
                 <div className="ExamBox"> {content.title} </div>
-                <div className="ExamBox"> {content.text} </div>
-            </div>
-            
-        );
-    return row;
-  }
 
+            </div>
+
+        );
+
+    return row;
+        
+  }
   componentDidMount() {
 
 
@@ -39,7 +109,7 @@ export default class FormPage extends React.Component {
         '<li id="chld"><a href="/profile">Profile</a></li>');
       _nav.insertAdjacentHTML('afterend',
         '<li id="chld"><a href="/exam">Exam</a></li>' +
-        '<li id="chld"><a href="/writingsList">My Writings</a></li>'+
+        '<li id="chld"><a href="/writingsList">My Writings</a></li>' +
         '<li id="chld"><a href="/writing">Send Writing</a></li>' +
         '<li id="chld"><a href="/messages">Messages</a></li>' +
         '<li id="chld" style="float:right";><a href="/Logout">Logout</a></li>' +
@@ -49,24 +119,47 @@ export default class FormPage extends React.Component {
   }
 
   render() {
-   
-    return (
-      <MDBContainer fluid>
-        <MDBRow className="topMargined">
-          <center><img className="backpicture" src=".\earth3.png" alt="." width="80%" /></center>
-          <MDBCol md="8">
-            <div className="marginedleft20">
-              <MDBRow>
-                <MDBCol md="12">
-                  <MDBRow>
-                        {this.fill_writing_table()}
-                  </MDBRow>
-                </MDBCol>
-              </MDBRow>
-            </div>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer >
-    );
+      console.log(this.state);
+
+    if(content.image === null){
+      return (
+        <MDBContainer fluid>
+          <MDBRow className="topMargined">
+            <center><img className="backpicture" src=".\earth3.png" alt="." width="80%" /></center>
+            <MDBCol>
+              <div className="marginedleft20">
+                <MDBRow>
+                  <MDBCol>
+                    <MDBRow><center>
+                      {this.textWriting()}
+                    </center></MDBRow>
+                  </MDBCol>
+                </MDBRow>
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer >
+      );
+    }
+    else{
+      return (
+        <MDBContainer fluid>
+          <MDBRow className="topMargined">
+            <center><img className="backpicture" src=".\earth3.png" alt="." width="80%" /></center>
+            <MDBCol>
+              <div className="marginedleft20">
+                <MDBRow>
+                  <MDBCol>
+                    <MDBRow><center>
+                      {this.imageWriting()}
+                    </center></MDBRow>
+                  </MDBCol>
+                </MDBRow>
+              </div>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer >
+      );
+    }
   }
 }
